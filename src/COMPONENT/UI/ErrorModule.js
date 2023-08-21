cc1909ed655d86231282fd8c13a787f4e8c37051
Amitgit1st <129199162+Amitgit1st@users.noreply.github.com>
2023-08-21 11:39:36 +0530

    Update ErrorModule.js (PORTAL)
    Edited code when we want to use createPORTAL

--- src/COMPONENT/UI/ErrorModule.js
+++ src/COMPONENT/UI/ErrorModule.js
@@ -1,25 +1,48 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Card from "./Card";
 import Button from './Button';
 import classes from './Error.module.css';
+import ReactDom from 'react-dom';
+
+const BackDrop = props => {
+    return <div className={classes.backdrop} onClick={props.onConfirm} />;
+};
+
+const ModalOverlay = props => {
+    return (
+    <Card className={classes.modal}>
+        <header className={classes.header} >
+            <h2>{props.title}</h2>
+        </header>
+        <div className={classes.content}>
+            <p>{props.message}</p>
+        </div>
+        <footer className={classes.actions}>
+            <Button onClick={props.onConfirm}>Okay</Button>
+        </footer>
+    </Card>
+    )
+}
 
 const ErrorModule = (props) => {
     return (
-        <div>
-            <div className={classes.backdrop} onClick={props.onConfirm} />
+        <React.Fragment>
 
-            <Card className={classes.modal}>
-                <header className={classes.header} >
-                    <h2>{props.title}</h2>
-                </header>
-                <div className={classes.content}>
-                    <p>{props.message}</p>
-                </div>
-                <footer className={classes.actions}>
-                    <Button onClick={props.onConfirm}>Okay</Button>
-                </footer>
-            </Card>
-        </div>
+            {ReactDom.createPortal(
+                <BackDrop onConfirm={props.onConfirm} />,
+                document.getElementById('backdrop-root')
+            )}
+
+            {ReactDom.createPortal(
+                <ModalOverlay
+                 title={props.title}
+                 message={props.message} 
+                 onConfirm={props.onConfirm}
+                 />,
+                 document.getElementById('overlay-root')
+            )}
+
+        </React.Fragment>
     )
 
 }
