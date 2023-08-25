18e267aeed15638e65491dc597e92ab2a0f1be50
Amitgit1st <129199162+Amitgit1st@users.noreply.github.com>
2023-08-25 22:55:24 +0530

    App.js(logOut - working)

--- Validating-the-forms-useEffect/App.js
+++ Validating-the-forms-useEffect/App.js
@@ -28,8 +28,14 @@ function App() {
 
   return (
 
-    <AuthContext.Provider value={{isLoggedIn: isLoggedIn}}>
-      <MainHeader onLogout={logoutHandler} />
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout : logoutHandler
+      }}>
+      {/* <MainHeader onLogout={logoutHandler} /> */}
+      
+      <MainHeader/>
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
